f086c459a52d119cd1c6e89294a701879315549b
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-03-14 12:49:27 -0400

    changed connection

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,7 @@
 const { Pool } = require("pg")
 
 const pool = new Pool ({
-    user: 'abdirahman',
-    host: 'localhost',
-    port: 5432,
-    database: 'inv'
+    connectionString: process.env.DATABASE_URL
 })
 
  module.exports = pool
